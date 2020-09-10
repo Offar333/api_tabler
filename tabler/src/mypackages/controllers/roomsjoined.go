ce8fa4d7eed2944e9cafcf34dc55aed540993f15
Rafael Vilas Boas de Carvalho <az.carta@gmail.com>
2020-09-10 16:44:50 -0300

    e la vamos nos

--- tabler/src/mypackages/controllers/roomsjoined.go
+++ tabler/src/mypackages/controllers/roomsjoined.go
@@ -36,7 +36,7 @@ func RoomsJoined(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(body, &keyVal)
 	userName := keyVal["ID_USUAR"]
 
-	result, err := db.Query("SELECT a.ID_MESA, a.TITULO_MESA, a.DESC_MESA , a.LVLINIC_MESA, a.EXPJOGO_MESA FROM mesa a "+
+	result, err := db.Query("SELECT a.ADM_MESA, a.ID_MESA, a.TITULO_MESA, a.DESC_MESA , a.LVLINIC_MESA, a.EXPJOGO_MESA, a.FORMA_MESA, a.LINK_CHAT, a.QTDEJOG_MESA, a.STATUS_MESA FROM mesa a "+
 		"INNER JOIN mesa_jogadores b ON a.ID_MESA = b.ID_MESA WHERE b.ID_USUAR = ?", userName)
 
 	if err != nil {
@@ -44,9 +44,21 @@ func RoomsJoined(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var room Room
+	/*
+		ID       int    `json:"id"`
+		AdmMesa  string `json:"admMesa"`
+		Title    string `json:"title"`
+		Desc     string `json:"desc"`
+		QtdeJog  string `json:"qtdeJog"`
+		Formato  string `json:"formato"`
+		Status   int    `json:"status"`
+		LvlInic  string `json:"lvlInic"`
+		ExpJogo  string `json:"expJogo"`
+		LinkChat string `json:"linkChat"`
+	*/
 
 	for result.Next() {
-		err := result.Scan(&room.ID, &room.Title, &room.Desc, &room.LvlInic, &room.ExpJogo)
+		err := result.Scan(&room.AdmMesa, &room.ID, &room.Title, &room.Desc, &room.LvlInic, &room.ExpJogo, &room.Formato, &room.LinkChat, &room.QtdeJog, &room.Status)
 		if err != nil {
 			panic(err.Error())
 		}
